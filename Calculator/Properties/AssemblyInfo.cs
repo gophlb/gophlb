38a7ad6cec01329d7b2a7cf6b6b718c09efea732
gophlb <gophlb@hotmail.com>
2014-05-25 16:43:04 +0200

    AssemblyTitle corrected

--- Calculator/Properties/AssemblyInfo.cs
+++ Calculator/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TreeTraversal")]
+[assembly: AssemblyTitle("Calculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TreeTraversal")]
+[assembly: AssemblyProduct("Calculator")]
 [assembly: AssemblyCopyright("Copyright 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

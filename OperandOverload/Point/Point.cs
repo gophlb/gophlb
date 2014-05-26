ee5c7928e9b4c3adc312099788386e9e3fe81a25
gophlb <gophlb@hotmail.com>
2014-05-26 17:37:08 +0200

    Operand Overload

---
+++ OperandOverload/Point/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OperandOverride
+{
+	public class Point
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+		
+		
+		public static Point operator + (Point point1, Point point2)
+		{
+			return new Point { X = point1.X + point2.X, Y = point1.Y + point2.Y };
+		}
+		
+		
+		public static Point operator - (Point point1, double delta)
+		{
+			return new Point { X = point1.X - delta, Y = point1.Y - delta };
+		}
+		
+		
+		public static double operator * (Point point1, double delta)
+		{
+			return (point1.X * delta) + (point1.Y * delta);
+		}		
+		
+		public static Point operator * (Point point1, Point point2)
+		{
+			return new Point { X = point1.X * point2.X, Y = point1.Y * point2.Y };
+		}
+		
+		
+		public Point() { }
+	}
+}
---
+++ OperandOverload/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace OperandOverride
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Point point1 = new Point { X = 1, Y = 2 };
+			Point point2 = new Point { X = 4, Y = 2 };
+			
+			Point point3 = point1 + point2;
+			Console.WriteLine("point3.X: {0}, point3.Y: {1}", point3.X, point3.Y);
+			
+			Point point4 = point1 - 5;
+			Console.WriteLine("point4.X: {0}, point4.Y: {1}", point4.X, point4.Y);
+						
+			Point point5 = new Point { X = 1.2, Y = 2.4 };
+			double multiplication = point5 * 5.0;
+			Console.WriteLine("multiplication: {0}", multiplication);
+			
+			Console.ReadKey(true);
+		}
+	}
+}
